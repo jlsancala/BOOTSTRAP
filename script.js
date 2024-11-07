68d12aab7efd11f1a2cce7d2281fca668879a202
jlsancala <sancalajohnlouis@gmail.com>
2024-11-07 03:12:30 +0000

    update

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-(function() {
-    emailjs.init("OW5mclnR7xtXXZpaO");
+(function(){
+    emailjs.init("BTpHwpVtO8gdZzP-l");
 })();
 
+
+// Function to send email using EmailJS
 document.getElementById('contact-form').addEventListener('submit', function(event) {
     event.preventDefault();
-
-    emailjs.sendForm('service_q0ph04k', 'template_3bs11sn', this)
+    // Send the email using the form data
+    console.log(this)
+    emailjs.sendForm('service_o9rk1pc', 'template_hrd9wkc', this)
         .then(function() {
             alert('Message Sent Successfully!');
         }, function(error) {
             alert('Failed to send the message: ' + JSON.stringify(error));
         });
 });
+
